b74eae0181581f60e88eb4cab32ebbee129e4c8e
huaweiwei <harvey0379@163.com>
2021-11-12 13:31:54 +0800

    add material-ui

--- leadmanager/frontend/src/App.jsx
+++ leadmanager/frontend/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function App() {
   const [leads, setLeads] = useState([]);
@@ -16,13 +23,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ul>
-          {leads.map((lead) => (
-            <li key={lead.id}>
-              {lead.name}-----{lead.email}
-            </li>
-          ))}
-        </ul>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="right">Name</TableCell>
+                <TableCell align="right">Email</TableCell>
+                <TableCell align="right">Message</TableCell>
+                <TableCell align="right">Created At</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {leads.map((row) => (
+                <TableRow
+                  key={row.name}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="right">{row.email}</TableCell>
+                  <TableCell align="right">{row.message}</TableCell>
+                  <TableCell align="right">{row.created_at}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </header>
     </div>
   );
