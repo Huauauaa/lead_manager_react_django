8380d07d8ed9e52cf9fa1368d56ca3ecf426fe59
huaweiwei <harvey0379@163.com>
2021-11-12 11:59:10 +0800

    implementing react

---
+++ leadmanager/frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [leads, setLeads] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/api/leads');
+      const data = await response.json();
+      setLeads(data);
+    })();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {leads.map((lead) => (
+            <li key={lead.id}>
+              {lead.name}-----{lead.email}
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
